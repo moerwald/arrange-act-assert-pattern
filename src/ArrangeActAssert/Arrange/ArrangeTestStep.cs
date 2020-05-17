0a269327c61d10f7b4e651997e6362faa947ee25
moerwald <andreas.mewald@gmail.com>
2020-05-17 12:50:32 +0200

    Introduced step base class.

--- src/ArrangeActAssert/Arrange/ArrangeTestStep.cs
+++ src/ArrangeActAssert/Arrange/ArrangeTestStep.cs
@@ -1,30 +1,16 @@
 ﻿using ArrangeActAssert.Context;
-using ArrangeActAssert.Test;
 using System;
 
 namespace ArrangeActAssert.Arrange
 {
-    public class ArrangeTestStep : IInvokeableTestStep
+    public sealed class ArrangeTestStep : TestStepBase<ArrangeException>
     {
-        private readonly IContext _context;
-        private readonly Action<IContext> _actionToInvoke;
-
         public ArrangeTestStep( IContext context, Action<IContext> actionToInvoke )
+            :base(context, actionToInvoke)
         {
-            _context = context;
-            _actionToInvoke = actionToInvoke;
         }
 
-        public void Invoke()
-        {
-            try
-            {
-                _actionToInvoke(_context);
-            }
-            catch (Exception exception)
-            {
-                throw new ArrangeException("Arrange step failed", exception);
-            }
-        }
+        protected override ArrangeException GetException(Exception innerException)
+            => new ArrangeException("[Arrange]: failed.", innerException);
     }
 }
---
+++ src/ArrangeActAssert/TestStepBase.cs
@@ -0,0 +1,35 @@
+﻿using ArrangeActAssert.Context;
+using ArrangeActAssert.Test;
+using System;
+
+namespace ArrangeActAssert
+{
+    public abstract class TestStepBase<TException> 
+        : IInvokeableTestStep
+        where TException : Exception
+    {
+        protected  IContext Context { get; }
+
+        protected Action<IContext> ActionToInvoke { get; }
+
+        public TestStepBase( IContext context, Action<IContext> actionToInvoke )
+        {
+            Context = context;
+            ActionToInvoke = actionToInvoke;
+        }
+
+        public void Invoke()
+        {
+            try
+            {
+                ActionToInvoke(Context);
+            }
+            catch (Exception exception)
+            {
+                throw GetException(exception);
+            }
+        }
+
+        protected abstract TException GetException(Exception innerException);
+    }
+}
