f669013ca301a01f5ac8636cc3ab54a6468979fc
moerwald <andreas.mewald@gmail.com>
2020-05-18 18:08:19 +0200

    Added new exception type.

---
+++ src/ArrangeActAssert/Assert/AssertException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArrangeActAssert.Asrt
+{
+    [Serializable]
+    public class AssertException : Exception
+    {
+        public AssertException() { }
+        public AssertException(string message) : base(message) { }
+        public AssertException(string message, Exception inner) : base(message, inner) { }
+        protected AssertException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
