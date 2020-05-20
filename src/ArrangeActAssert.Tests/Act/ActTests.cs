cb0656bca418436f2f30bfbd0ca6c6607aff0645
moerwald <andreas.mewald@gmail.com>
2020-05-20 09:53:13 +0200

    Act tests are now green

---
+++ src/ArrangeActAssert.Tests/Act/ActTests.cs
@@ -0,0 +1,37 @@
+﻿using ArrangeActAssert.Act;
+using NUnit.Framework;
+using System;
+
+namespace ArrangeActAssert.Tests.Arrange
+{
+    [TestFixture]
+    public sealed class ActTests
+    {
+        [Test]
+        public void Act_ExceptionIsThrown_ExceptionRethrownAsArrangeException()
+        {
+            Assert.Throws<ActException>(
+                () =>
+                    Pattern.New
+                            .Arrange(c => { })
+                            .Act(x => throw new Exception("Act failed"))
+                            .Assert(x => { })
+                            .Invoke()
+                            );
+        }
+
+        [Test]
+        public void Act_NoError_ActionWasInvoked()
+        {
+            var actStepCalled = false;
+            Pattern.New
+                    .Arrange(c => { })
+                    .Act(x => actStepCalled = true)
+                    .Assert(x => { })
+                    .Invoke();
+
+            Assert.IsTrue(actStepCalled);
+        }
+
+    }
+}
