0354da7f7265309f41a1f20d3bd4d67367ee07c4
moerwald <andreas.mewald@gmail.com>
2020-05-22 10:32:18 +0200

    Removed useless using statements.

--- src/ArrangeActAssert/Test/Invoker/IInvoke.cs
+++ src/ArrangeActAssert/Test/Invoker/IInvoke.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ArrangeActAssert.Test.Invoker
+﻿namespace ArrangeActAssert.Test.Invoker
 {
     public interface IInvoke
     {
