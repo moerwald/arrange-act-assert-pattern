6c5c2e7da149a0cd8f28c5530fcbd02b9a4bc2bc
moerwald <andreas.mewald@gmail.com>
2020-05-22 10:23:30 +0200

    Refactored test cases.

--- src/ArrangeActAssert.Tests/Context/DefaultContextTests.cs
+++ src/ArrangeActAssert.Tests/Context/DefaultContextTests.cs
@@ -7,54 +7,56 @@ namespace ArrangeActAssert.Tests.Context
     [TestFixture]
     public sealed class DefaultContextTests
     {
+        private DefaultContext _ctx;
+
+        [SetUp]
+        public  void Setup()
+        {
+            _ctx = new DefaultContext();
+        }
+
         [Test]
         public void Add_InputIsValid_DontThrowException()
         {
-            var ctx = new DefaultContext();
-            Assert.DoesNotThrow(() => ctx.Add("someName", 1));
+            Assert.DoesNotThrow(() => _ctx.Add("someName", 1));
         }
 
         [Test]
         public void Get_ParmeterNotFound_ThrowInvalidArgumentException()
         {
-            var ctx = new DefaultContext();
             Assert.Throws<ArgumentException>(
-                () => ctx.Get<int>("invalidName")
+                () => _ctx.Get<int>("invalidName")
                 );
         }
 
         [Test]
         public void Get_TypeDoesntMatchStoreOne_ThrowInvalidCastException()
         {
-            var ctx = new DefaultContext();
-            ctx.Add("a", 1);
+            _ctx.Add("a", 1);
             Assert.Throws<InvalidCastException>(
-                () => ctx.Get<string>("a")
+                () => _ctx.Get<string>("a")
                 );
         }
 
         [Test]
         public void Get_TypeMatchesStoredOne_CorrectValueIsReturned()
         {
-            var ctx = new DefaultContext();
-            ctx.Add("a", 1);
-            Assert.AreEqual(1, ctx.Get<int>("a"));
+            _ctx.Add("a", 1);
+            Assert.AreEqual(1, _ctx.Get<int>("a"));
         }
 
         [Test]
         public void GetSystemUnderTest_SutWasSetBeforehand_CorrectValueIsReturned()
         {
-            var ctx = new DefaultContext();
-            ctx.SetSystemUnderTest("System under Test");
-            Assert.AreEqual("System under Test", ctx.GetSystemUnderTest<string>());
+            _ctx.SetSystemUnderTest("System under Test");
+            Assert.AreEqual("System under Test", _ctx.GetSystemUnderTest<string>());
         }
 
         [Test]
         public void GetSystemUnderTest_SutWasNOTSetBeforehand_ThrowInvalidArgumentException()
         {
-            var ctx = new DefaultContext();
             Assert.Throws<ArgumentException>(
-                () => ctx.GetSystemUnderTest<string>()
+                () => _ctx.GetSystemUnderTest<string>()
                 );
         }
     }
