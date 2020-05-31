c49f327a1c387983d5434660e8f21852efa84405
moerwald <andreas.mewald@gmail.com>
2020-05-31 09:29:28 +0200

    - Added test showing exception output.

---
+++ src/ArrangeActAssert.ShowCases.Tests/SimpleTests.cs
@@ -0,0 +1,93 @@
+﻿using ArrangeActAssert.Act;
+using NUnit.Framework;
+using System;
+
+namespace ArrangeActAssert.Tests.ShowCases
+{
+    [TestFixture]
+    public class SimpleTests
+    {
+        [Test]
+        public void AddTwoNumbersUsingContext()
+            => Pattern.Configure
+                      .MeasureExecutionTime()
+                      .New
+                      .Arrange(ctx =>
+                      {
+                          ctx.Add("a", 1);
+                          ctx.Add("b", 2);
+                      })
+                      .Act(ctx =>
+                      {
+                          var a = ctx.Get<int>("a");
+                          var b = ctx.Get<int>("b");
+                          var result = a + b;
+
+                          ctx.Add("result", result);
+                      })
+                      .Assert(ctx =>
+                      {
+                          var result = ctx.Get<int>("result");
+                          Assert.AreEqual(3, result);
+                      })
+                      .Run();
+
+        [Test]
+        public void AddTwoNumbersUsingClosures()
+        {
+            int a = 0;
+            int b = 0;
+            int result = 0;
+            Pattern.Configure
+                     .MeasureExecutionTime()
+                     .New
+                     .Arrange(ctx =>
+                     {
+                         a = 1;
+                         b = 2;
+                     })
+                     .Act(ctx =>
+                     {
+                         result = a + b;
+                     })
+                     .Assert(ctx =>
+                     {
+                         Assert.AreEqual(3, result);
+                     })
+                     .Run();
+        }
+
+        [Test]
+        public void AddTwoNumbersUsingContext_ActThrowException_ExceptionConvertedToActException()
+        {
+            bool actExceptionThrown = false;
+            try
+            {
+
+                Pattern.Configure
+                         .MeasureExecutionTime()
+                         .New
+                         .Arrange(ctx =>
+                         {
+                             ctx.Add("a", 1);
+                             ctx.Add("b", 2);
+                         })
+                         .Act(ctx =>
+                         {
+                             throw new Exception("Act failed");
+                         })
+                         .Assert(ctx =>
+                         {
+                         })
+                         .Run();
+            }
+            catch (ActException exception)
+            {
+                Console.WriteLine(exception.ToString());
+                actExceptionThrown = true;
+            }
+
+            Assert.IsTrue(actExceptionThrown);
+        }
+    }
+}
